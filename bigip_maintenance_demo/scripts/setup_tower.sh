c1d39753783ca5fa0e56140b242110446a2013d2
Masatoshi Murai <mamurai@redhat.com>
2019-06-20 04:44:41 -0400

    Add Webserver Maintenance

--- bigip_maintenance_demo/scripts/setup_tower.sh
+++ bigip_maintenance_demo/scripts/setup_tower.sh
@@ -19,4 +19,14 @@ tower-cli project create --name="F5-Demo" --description="F5-Demo Project" --scm-
 ## Job Template ##
 tower-cli job_template create --name="bigip-config-add" --description="Add Big-ip config" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_config_add.yml"
 tower-cli job_template create --name="bigip-config-delete" --description="Delete Big-ip config" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_config_delete.yml"
+tower-cli job_template create --name="Disabled_Pool-member" --description="Disabled Pool Member" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_disabled_pool-member.yml"
+tower-cli job_template create --name="Enabled_Pool-member" --description="Enabled Pool Member" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_enabled_pool-member.yml"
+tower-cli job_template create --name="Webserver1-Maintenance" --description="Webserver1 Maintenance" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/host1_maintenance.yml"
 
+## Workflow ##
+tower-cli workflow create --name="WF-Webserver-Maintenance" --organization="F5-Demo" --description="Webserver Maintenance Demo Workflow"
+tower-cli node create -W WF-Webserver-Maintenance --job-template="Disabled_Pool-member"
+tower-cli node create -W WF-Webserver-Maintenance --job-template="Webserver1-Maintenance"
+tower-cli node create -W WF-Webserver-Maintenance --job-template="Enabled_Pool-member"
+tower-cli node associate_success_node 1 2
+tower-cli node associate_success_node 2 3
