80205d5f49ce206380a5deb7f760cbae2b14ddae
mamurai <mamurai@redhat.com>
2019-06-21 11:12:00 +0900

    Update setup_tower.sh

--- bigip_maintenance_demo/scripts/setup_tower.sh
+++ bigip_maintenance_demo/scripts/setup_tower.sh
@@ -16,12 +16,15 @@ sudo tower-manage inventory_import --source=/home/student1/networking-workshop/l
 ## Project ##
 tower-cli project create --name="F5-Demo" --description="F5-Demo Project" --scm-type="git" --scm-url="https://github.com/mamurai/Ansible_Demos" --organization="F5-Demo" --wait
 
+## Credential ##
+tower-cli credential create --name="F5-Demo-Credential" --organization="F5-Demo" --inputs="{"username": "${USER}"}" --credential-type="Machine"
+
 ## Job Template ##
 tower-cli job_template create --name="bigip-config-add" --description="Add Big-ip config" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_config_add.yml"
 tower-cli job_template create --name="bigip-config-delete" --description="Delete Big-ip config" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_config_delete.yml"
 tower-cli job_template create --name="Disabled_Pool-member" --description="Disabled Pool Member" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_disabled_pool-member.yml"
 tower-cli job_template create --name="Enabled_Pool-member" --description="Enabled Pool Member" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/bigip_enabled_pool-member.yml"
-tower-cli job_template create --name="Webserver1-Maintenance" --description="Webserver1 Maintenance" --inventory="F5-Demo-Inventory" --project="F5-Demo" --playbook="${BASE_DIR}/host1_maintenance.yml"
+tower-cli job_template create --name="Webserver1-Maintenance" --description="Webserver1 Maintenance" --inventory="F5-Demo-Inventory" --credential="F5-Demo-Credential" --project="F5-Demo" --playbook="${BASE_DIR}/host1_maintenance.yml"
 
 ## Workflow ##
 tower-cli workflow create --name="WF-Webserver-Maintenance" --organization="F5-Demo" --description="Webserver Maintenance Demo Workflow"
