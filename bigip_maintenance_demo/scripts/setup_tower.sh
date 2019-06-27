a63f434aec622a23af14e8700ea2a75f639036e9
mamurai <mamurai@redhat.com>
2019-06-27 11:36:28 +0900

    Update setup_tower.sh

--- bigip_maintenance_demo/scripts/setup_tower.sh
+++ bigip_maintenance_demo/scripts/setup_tower.sh
@@ -9,6 +9,24 @@ tower-cli login admin
 ## Organization ##
 tower-cli organization create --name="F5-Demo"
 
+## Team ##
+tower-cli team create --name="Ops" --organization=F5-Demo --description="The Ops Team"
+tower-cli team create --name="Infra" --organization=F5-Demo --description="The Infra Team"
+
+## User ##
+tower-cli user create --username="ops01" --password="password" --email=mamurai@redhat.com --first-name=”Taro” --last-name=”Ops”
+tower-cli team associate --team=Ops --user=ops01
+
+tower-cli user create --username="ops02" --password="password" --email=mamurai@redhat.com --first-name=”Jiro” --last-name=”Ops”
+tower-cli team associate --team=Ops --user=ops02
+
+tower-cli user create --username="infra01" --password="password" --email=mamurai@redhat.com --first-name=”Taro” --last-name=”Infra”
+tower-cli team associate --team=Infra --user=infra01
+
+tower-cli user create --username="infra02" --password="password" --email=mamurai@redhat.com --first-name=”Jiro” --last-name=”Infra”
+tower-cli team associate --team=Infra --user=infra02
+
+
 ## Inventory ##
 tower-cli inventory create --name="F5-Demo-Inventory" --organization="F5-Demo"
 sudo tower-manage inventory_import --source=/home/${USER}/networking-workshop/lab_inventory/hosts --inventory-name="F5-Demo-Inventory"
@@ -33,3 +51,12 @@ tower-cli node create -W WF-Webserver-Maintenance --job-template="Webserver1-Mai
 tower-cli node create -W WF-Webserver-Maintenance --job-template="Enabled_Pool-member"
 tower-cli node associate_success_node 1 2
 tower-cli node associate_success_node 2 3
+
+## Role ##
+tower-cli role grant --team=Infra --type=execute --job-template=bigip-config-add
+tower-cli role grant --team=Infra --type=execute --job-template=bigip-config-delete
+
+tower-cli role grant --team=Ops --type=execute --workflow=WF-Webserver-Maintenance
+tower-cli role grant --team=Ops --type=read --job-template=Disabled_Pool-member
+tower-cli role grant --team=Ops --type=read --job-template=Enabled_Pool-member
+tower-cli role grant --team=Ops --type=read --job-template=Webserver1-Maintenance
